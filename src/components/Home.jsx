634a64021f3f7c30e86d25ce2b2d6455e392a696
melvin-Code <melvingonzalez470@gmail.com>
2020-08-27 16:53:07 -0400

    check

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,12 @@ class Home extends Component {
     render() {
         return (
             <div>
-                
+                <div>
+                    <h2></h2>
+                    <div>
+                        <h3></h3>
+                    </div>
+                </div>
             </div>
         );
     }
