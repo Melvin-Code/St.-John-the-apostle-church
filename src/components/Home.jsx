6ac583b4590f23bd216ef98f5904ddee98c6c19b
melvin-Code <melvingonzalez470@gmail.com>
2020-08-27 17:01:40 -0400

    home clasess added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,19 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <div>
-                <div>
-                    <h2></h2>
-                    <div>
-                        <h3></h3>
+            <div className= 'homePage'>
+                <div className='hp-herocontainer'>
+                    <h2 className='herocontainer-slogan'></h2>
+                    <div className='herocontainer-welcomecontainer'>
+                        <h3 className='herocontainer-welcometext'></h3>
+                    </div>
+                </div>
+                <div className='homepage-links'>
+                    <div className='hp-link-container'>
+                        <div className='hp-link-onhoverscreen'>
+                            <p className='onhoverscreen-text'></p>
+                            <button className='onhoverscreen-button'></button>
+                        </div>
                     </div>
                 </div>
             </div>
