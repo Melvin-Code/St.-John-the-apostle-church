f2eb82d42d9fcc2dfd03f905646f9efe5c753f4f
melvin-Code <melvingonzalez470@gmail.com>
2020-09-10 16:24:31 -0400

    hero image done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className= 'homePage'>
-                <div className='hp-herocontainer'>
-                    <h2 className='herocontainer-slogan'></h2>
-                    <div className='herocontainer-welcomecontainer'>
-                        <h3 className='herocontainer-welcometext'></h3>
-                    </div>
-                </div>
-                <div className='homepage-links'>
-                    <div className='hp-link-container'>
-                        <div className='hp-link-onhoverscreen'>
-                            <p className='onhoverscreen-text'></p>
-                            <button className='onhoverscreen-button'></button>
-                        </div>
-                    </div>
-                </div>
+  leslie(){
+    
+  }
+  render() {
+    return (
+      <div className="homePage">
+        <div className="hp-herocontainer">
+          <h2 className="herocontainer-slogan">
+            ONE HEART, ONE FAITH, ONE CHURCH
+          </h2>
+          <div className="herocontainer-welcomecontainer">
+            <h3 className="herocontainer-welcometext">WELCOME</h3>
+            <p className="herocontainer-welcometext--sub">
+              We are the community of St. John the Apostle Catholic Church,
+              located in Hialeah, Florida since 1945 We are a welcoming and
+              loving community rooted in the gospels of Jesus Christ, and value
+              the teachings of our Lord. We are committed to value all human
+              beings with dignity, self worth and to recognize the perfectly
+              created beauty God gave us within each other.
+            </p>
+          </div>
+        </div>
+        <div className="homepage-links">
+          <div className="hp-link-container">
+            <div className="hp-link-onhoverscreen">
+              <p className="onhoverscreen-text"></p>
+              <button className="onhoverscreen-button"></button>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+      
+    );
+  }
 }
 
 export default Home;
