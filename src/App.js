101c3110afb224fe755c49e458fcb1f3a3475734
melvin-Code <melvingonzalez470@gmail.com>
2020-09-28 21:21:31 -0400

    homepage structure done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home'
 import NavBar from './components/NavBar'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+
 import Footer from "./partials/Footer";
 import Hotlinks from "./partials/Hotlinks";
 
